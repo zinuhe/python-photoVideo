eedbeb35a465045609c7d1448e4400e76ef7b3b4
Jimmy <zinuhe@outlook.com>
2021-07-14 22:22:01 -0400

    allow only photo files in a list

--- processPhotoVideoSony.py
+++ processPhotoVideoSony.py
@@ -6,8 +6,9 @@
 # TODO
 # -linea 60 de donde saca esa fecha? Porque no usa la fecha de EXIF (parece que ya quedo, probar mas)
 # -DONE--linea 59 si son mas de 999 falla, por el i:03 leer primero cuantos files hay en el folder
+# -DONE--esta leyendo todos los archivos solo deberia leer photo files, add PHOTO_TYPES
 # -a la segunda vez, si algun archivo fue removido no funciona bien, pierde el conteo
-# -preguntar por parametro si usar _event_ o algo diferente
+# -preguntar por parametro si usar "_event_" o algo diferente
 
 import os, shutil, glob, sys
 import exifread #pip3 install exifread
@@ -15,6 +16,9 @@ import os.path, time, calendar
 from subprocess import check_output, check_call
 from datetime import datetime
  
+# Photo files extensions allowed
+PHOTO_TYPES = ['.DNG', '.JPE', '.JPEG', '.JPG', '.PNG']
+
 # Current working directory
 currentPath = os.getcwd() + "/"
 
@@ -37,46 +41,53 @@ for folder in folders:
     smallestDatetime = datetime(2100, 12, 31)
 
     for i, file in enumerate(files, start=1):
-        # Open image file for reading (binary mode)
-        f = open(currentPath + folder + "/" + file, 'rb')
+        # get file extension
+        fileExtension = os.path.splitext(file)[1]
+        # print(f"fileExtension: {fileExtension}")
+        
+        # Filter only PHOTO_TYPES files
+        if fileExtension.upper() in PHOTO_TYPES:
+            # Open image file for reading (binary mode)
+            f = open(currentPath + folder + "/" + file, 'rb')
 
-        # Return Exif tags
-        tags = exifread.process_file(f, details=False, stop_tag='EXIF DateTimeDigitized')
+            # Return Exif tags
+            tags = exifread.process_file(f, details=False, stop_tag='EXIF DateTimeDigitized')
 
-        if bool(tags):
-            # YYYY:MM:DD H:M:S
-            dateFromExif = tags['EXIF DateTimeDigitized']
-            # print(f"dateFromExif: {dateFromExif}")
+            if bool(tags):
+                # YYYY:MM:DD H:M:S
+                dateFromExif = tags['EXIF DateTimeDigitized']
+                # print(f"dateFromExif: {dateFromExif}")
 
-            # convert to datetime - 2021-07-09 09:42:07
-            dateTimeFromExif = datetime.strptime(str(dateFromExif), '%Y:%m:%d %H:%M:%S')
-            #print(f"{dateTimeFromExif.year}-{dateTimeFromExif.month}-{dateTimeFromExif.day}"")
+                # convert to datetime - 2021-07-09 09:42:07
+                dateTimeFromExif = datetime.strptime(str(dateFromExif), '%Y:%m:%d %H:%M:%S')
+                #print(f"{dateTimeFromExif.year}-{dateTimeFromExif.month}-{dateTimeFromExif.day}"")
 
-            # smallestDatetime, store and compare
-            # print(f"Type dateFromExif: {type(dateFromExif)}")
-            # print(f"Type dateTimeFromExif: {type(dateTimeFromExif)}")
-            if dateTimeFromExif < smallestDatetime:
-                smallestDatetime = dateTimeFromExif
+                # smallestDatetime, store and compare
+                # print(f"Type dateFromExif: {type(dateFromExif)}")
+                # print(f"Type dateTimeFromExif: {type(dateTimeFromExif)}")
+                if dateTimeFromExif < smallestDatetime:
+                    smallestDatetime = dateTimeFromExif
 
-            # YYYY-MM-DD_event_001.ext
-            ## newFileName = time.strftime('%Y-%m-%d_event_', time.localtime(os.path.getmtime(currentPath + folder))) + f'{i:03}'
-            newFileName = dateTimeFromExif.strftime('%Y-%m-%d') + "_event_" + f"{i:{_secuence}}"
-            #print(f"newFileName: {newFileName}")
+                # YYYY-MM-DD_event_001.ext
+                ## newFileName = time.strftime('%Y-%m-%d_event_', time.localtime(os.path.getmtime(currentPath + folder))) + f'{i:03}'
+                newFileName = dateTimeFromExif.strftime('%Y-%m-%d') + "_event_" + f"{i:{_secuence}}"
+                #print(f"newFileName: {newFileName}")
 
-            fileExtension = os.path.splitext(file)[1]
-            #print(f"fileExtension: {fileExtension}")
+                newFileName = newFileName + fileExtension
+                #print(f"newFileName: {newFileName}")
 
-            newFileName = newFileName + fileExtension
-            #print(f"newFileName: {newFileName}")
-
-            if file != newFileName:
-                # Renaming files
-                try:
-                    os.rename(folder + "/" + file, folder + "/" + newFileName)
-                    #print(f"Renaming file: {folder}/{file} --> {folder}/{newFileName}")
-                except OSError:
-                    print(f"Renaming file operation failed: {folder}/{file} --> {folder}/{newFileName}")
-                    sys.exit()
+                if file != newFileName:
+                    # Renaming files
+                    try:
+                        os.rename(folder + "/" + file, folder + "/" + newFileName)
+                        #print(f"Renaming file: {folder}/{file} --> {folder}/{newFileName}")
+                    except OSError:
+                        print(f"Renaming file operation failed: {folder}/{file} --> {folder}/{newFileName}")
+                        sys.exit()
+            else:
+                print(f"No EXIF data for: '{folder}/{file}' file was not rename")
+        else:
+            print(f"Not a photo file '{folder}/{file}'")
 
 
     #print(f"smallestDatetime: {smallestDatetime}")
@@ -91,7 +102,7 @@ for folder in folders:
                 #print(f"Renaming folder: {folder} --> {newFolderName}")
                 os.rename(folder, newFolderName)
             except OSError:
-                print(f"Renaming folder operation failed: {folder}/{file} --> {folder}/{newFileName}")
+                print(f"Renaming folder operation failed: {folder} --> {newFolderName}")
                 sys.exit()
     else:
         print(f"ERROR - Getting new folder name for {folder} fails - probably date is not valid")
