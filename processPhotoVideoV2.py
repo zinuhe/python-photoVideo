74af5b9c6f8b65cba7eea29ee5f91798b3719bdc
Jimmy <zinuhe@outlook.com>
2022-02-16 01:01:18 -0500

    refactoring

--- processPhotoVideoV2.py
+++ processPhotoVideoV2.py
@@ -1,4 +1,4 @@
-# Python3 ~/Documents/DEV/Python/SourceCode/PhotoVideo/processPhotoVideo.py
+# Python3 ~/Documents/DEV/Python/SourceCode/PhotoVideo/processPhotoVideoV2.py
 # To process photo and video files under same folder
 # By example files from iPhone
 
@@ -13,6 +13,14 @@
 #renombrarlos ordenadamente por hora
 ##NO ESTA FUNCIONANDO BIEN, cuando funciona no quedan bien ordenados
 
+##EL PROBLEMA ES QUE CUANDO HACE EL SORT LO HACE POR NOMBRE EN LUGAR DE
+## FECHA CREACION o MODIFICATION
+
+##Solucion
+#Crear un array con nombre, extension, fecha creacion EXIF, fecha creacion file
+#Ordenar ese array por fecha creacion EXIF si no existe usar fecha creacion file
+
+
 import os, shutil, glob
 import exifread #pip install exifread
 import os.path, time, calendar
@@ -23,6 +31,17 @@ from datetime import datetime
 PHOTO_TYPES = ('*.dng', '*.DNG', '*.jpe', '*.JPE', '*.jpeg', '*.JPEG', '*.jpg', '*.JPG', '*.png', '*.PNG')
 VIDEO_TYPES = ('*.mov', '*.MOV', '*.mp4', '*.MP4')
 EVENT_NAME = "_event_"
+
+
+class file:
+    def __init__(self, name, ext, exifCreation, exifModification, fileCreation, fileModification):
+        self.name = name
+        self.ext = ext
+        self.exifCreation = exifCreation
+        self.exifModification = exifModification
+        self.fileCreation = fileCreation
+        self.fileModification = fileModification
+
 
 # Creates a new folder and return a boolean
 def createFolder(pathNewFolder):
@@ -48,9 +67,47 @@ def getNameFiles(p_extensionFiles):
     for ext in p_extensionFiles:
         filesNames.extend(glob.glob(ext))
 
-    filesNames.sort()
+    # filesNames.sort()
     # print(f"Files Names sort: {filesNames}")
     return filesNames
+
+
+#WORKING HERE
+def getFileInfo(listFilesNames):
+    files = []
+
+    for item in listFilesNames:
+        # Open media file for reading (binary mode)
+        f = open(currentPath + item, 'rb')
+
+        # Return Exif tags
+        tags = exifread.process_file(f, details=False, stop_tag='EXIF DateTimeDigitized')
+
+        try:
+            if bool(tags):
+                #print(f"tags['EXIF DateTimeOriginal']: {tags['EXIF DateTimeOriginal']}")
+                dateFromExif = tags['EXIF DateTimeDigitized']
+                # It needs to be converted to datetime - 2021-07-15 09:42:07
+                dateFrom = datetime.strptime(str(dateFromExif), '%Y:%m:%d %H:%M:%S')
+            else: #Not EXIF Info
+                # get creation/modification date from the file
+                # creationTime     = time.strftime('%Y-%m-%d', time.localtime(os.path.getctime(currentPath + file)))
+                # modificationTime = time.strftime('%Y-%m-%d', time.localtime(os.path.getmtime(currentPath + file)))
+                dateFromCreationDate = time.strftime('%Y-%m-%d', time.localtime(os.path.getmtime(currentPath + file)))
+                dateFrom = datetime.strptime(str(dateFromCreationDate), '%Y-%m-%d')
+        except OSError:
+            print(f"Path {currentPath} does not exist or is inaccessible")
+            sys.exit()
+
+        # appending instances to list files
+        files.append(file(item, item, dateFrom, '', '', ''))
+
+    print(f"FILES")
+    x = sorted(files, key=lambda file:file.exifCreation)   # sort by exifCreation
+    for f in x:
+        print(f"{f.name}, {f.ext}, {f.exifCreation}, {f.exifModification}, {f.fileCreation}, {f.fileModification}")
+
+    return files
 
 # Iterates throught files and moves them
 def processMediaFiles(mediaFiles, mediaPath):
@@ -138,12 +195,15 @@ videoPath = currentPath + "video"
 photoFiles = getNameFiles(PHOTO_TYPES) #returns an array with photo files
 videoFiles = getNameFiles(VIDEO_TYPES) #returns an array with video files
 
-if len(photoFiles) > 0:
-    createFolder(photoPath)
-    # Process photo files
-    processMediaFiles(photoFiles, photoPath)
+#NEW
+photoFiles2 = getFileInfo(photoFiles) #returns an array with photo files
 
-if len(videoFiles) > 0:
-    createFolder(videoPath)
-    # Process video files
-    processMediaFiles(videoFiles, videoPath)
+# if len(photoFiles) > 0:
+#     createFolder(photoPath)
+#     # Process photo files
+#     processMediaFiles(photoFiles, photoPath)
+#
+# if len(videoFiles) > 0:
+#     createFolder(videoPath)
+#     # Process video files
+#     processMediaFiles(videoFiles, videoPath)
