710e8cd8d7d7bd159f315d99f7703b5a629be60c
Jimmy <zinuhe@outlook.com>
2022-11-01 23:55:29 -0400

    Updating

--- processFoldersDates.py
+++ processFoldersDates.py
@@ -1,19 +1,27 @@
-# Python3 ~/Documents/DEV/Python/SourceCode/PhotoVideo/processPhotoVideoV2.py
-# To process photo and video files under same folder
-# By example files from iPhone
+# Python3 ~/Documents/DEV/Python/SourceCode/PhotoVideo/processFoldersDates.py
+# Python3 processFoldersDates.py
+# To set up folders creation dates base on inside files
 
 #TODO
+#Todo
+#Go into a folder
+#get files inside and get creation dates
+#get the older file date
+#set up the upper folder with that date
+#Add _month_day at the end of folder's name?
 
 import os, shutil, glob
 import exifread #pip install exifread
-import os.path, time, calendar
+import os.path, time, datetime, calendar
 from subprocess import check_output, check_call
-from datetime import datetime
+# from datetime import datetime
+from icecream import ic #pip install icecream
 
 # Photo and Video files extensions allowed
 PHOTO_TYPES = ('*.dng', '*.DNG', '*.jpe', '*.JPE', '*.jpeg', '*.JPEG', '*.jpg', '*.JPG', '*.png', '*.PNG')
 VIDEO_TYPES = ('*.mov', '*.MOV', '*.mp4', '*.MP4')
 EVENT_NAME = "event_"
+FOLDER_NAME = "FOLDER_NAME"
 
 # To manage object file
 class file:
@@ -26,22 +34,6 @@ class file:
         self.fullDate = fullDate
         self.exifCreation = exifCreation
 
-# Creates a new folder and return a boolean
-def createFolder(pathNewFolder):
-    isCreatedOrExists = False
-    if os.path.isdir(pathNewFolder) == False:
-        try:
-            os.mkdir(pathNewFolder)
-        except OSError:
-            print(f"Creation of the directory {pathNewFolder} failed")
-        else:
-            isCreatedOrExists = True
-    else:
-        # print(f"Folder '{pathNewFolder}' already exists")
-        isCreatedOrExists = True
-
-    return isCreatedOrExists
-
 
 # Gets file extensions and return an array of matching files
 def getNameFiles(p_extensionFiles):
@@ -50,6 +42,22 @@ def getNameFiles(p_extensionFiles):
         filesNames.extend(glob.glob(ext))
 
     return filesNames
+
+def getOldestDateFromFiles(listFilesNames):
+    oldestDate = datetime.datetime.today()
+
+    for fileName in listFilesNames:
+        c_time = os.stat(currentPath + fileName).st_birthtime
+        dt_c = datetime.datetime.fromtimestamp(c_time)
+        if oldestDate > dt_c:
+            oldestDate = dt_c
+
+    return oldestDate
+
+
+# # Set the proper date to the new folder just created
+# tmpCreationDate =  file.monthNumber + "/" + file.day + "/" + file.year + " 01:00" #"12/20/2020 16:13"
+# check_call(['Setfile', '-d', tmpCreationDate, mediaPath + "/" + file.year + "/" + newDate])
 
 # Gets the year from a given date
 def getYear(inputDate):
@@ -70,81 +78,8 @@ def getDay(inputDate):
 def getFullDate(inputDate):
     return getYear(inputDate) + getMonthNumber(inputDate) + getDay(inputDate)
 
-#WORKING HERE
-# Return a list of 'file' objects sorted by creation/modification date
-def getFileInfo(listFilesNames):
-    files = []
 
-    for fileName in listFilesNames:
-        # Open media file for reading (binary mode)
-        f = open(currentPath + fileName, 'rb')
-
-        # Return Exif tags
-        tags = exifread.process_file(f, details=False, stop_tag='EXIF DateTimeDigitized')
-
-        try:
-            if bool(tags):
-                #print(f"tags['EXIF DateTimeOriginal']: {tags['EXIF DateTimeOriginal']}")
-                dateFromExif = tags['EXIF DateTimeDigitized']
-                # It needs to be converted to datetime - 2022-09-28 19:40:07
-                dateFrom = datetime.strptime(str(dateFromExif), '%Y:%m:%d %H:%M:%S')
-            else: #Not EXIF Info - get creation/modification date from the file
-                # creationDate = time.strftime('%Y-%m-%d', time.localtime(os.path.getctime(currentPath + fileName)))
-                modificationDate = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.path.getmtime(currentPath + fileName)))
-                dateFrom = datetime.strptime(str(modificationDate), '%Y-%m-%d %H:%M:%S')
-        except OSError:
-            print(f"Path {currentPath} does not exist or is inaccessible")
-            sys.exit()
-
-        # appending instances to list files
-        files.append(file(fileName, getYear(dateFrom), getMonthNumber(dateFrom), getMonthName(dateFrom), getDay(dateFrom), getFullDate(dateFrom), dateFrom))
-
-    sortedFiles = sorted(files, key=lambda file:file.exifCreation) # sort by exifCreation
-    # for f in sortedFiles:
-    #     print(f"{f.name}, {f.year}, {f.monthNumber}, {f.monthName}, {f.day}, {f.fullDate},  {f.exifCreation}")
-
-    return sortedFiles
-
-# Iterates throught files and moves them
-def processMediaFiles(mediaFiles, mediaPath):
-    index = 1
-
-    for i, file in enumerate(mediaFiles, start=1):
-        # Creates subfolder with the year if it doesn't already exists
-        createFolder(mediaPath + "/" + file.year)
-        check_call(['Setfile', '-d', "01/01/" + file.year + " 01:00", mediaPath + "/" + file.year])
-
-        if i == 1:
-            # Keep date to restart index
-            keepDate = file.fullDate
-        elif keepDate == file.fullDate:
-            index += 1
-        else:
-            index = 1
-            keepDate = file.fullDate
-
-        newDate = EVENT_NAME + file.monthName + "-" + file.day
-
-        if newDate != '':
-            if createFolder("/".join((mediaPath, file.year, newDate))):
-                # Set the proper date to the new folder just created
-                tmpCreationDate =  file.monthNumber + "/" + file.day + "/" + file.year + " 01:00" #"12/20/2020 16:13"
-                check_call(['Setfile', '-d', tmpCreationDate, mediaPath + "/" + file.year + "/" + newDate])
-
-                fileExtension = os.path.splitext(file.name)[1]
-
-                newFileName = "-".join((file.year, file.monthNumber, file.day))
-                newFileName = "".join((newFileName, "_", EVENT_NAME, f'{index:03}', fileExtension)) # {index:03} To add secuency
-
-                os.rename(file.name, newFileName)
-
-                # Move the file (using new name) to the new folder
-                shutil.move(newFileName, "/".join((mediaPath, file.year, newDate)))
-        else:
-            print(f"DATE {newDate} NOT VALID")
-
-    return
-
+# PROGRAM STARTS
 # Gets the current working directory
 currentPath = os.getcwd() + "/"
 
@@ -158,14 +93,6 @@ videoPath = currentPath + "video"
 photoFiles = getNameFiles(PHOTO_TYPES) #returns an array with photo files
 videoFiles = getNameFiles(VIDEO_TYPES) #returns an array with video files
 
-if len(photoFiles) > 0:
-    createFolder(photoPath)
-    orderedPhotoFiles = getFileInfo(photoFiles)
-    # Process photo files
-    processMediaFiles(orderedPhotoFiles, photoPath)
+# ic(photoFiles)
 
-if len(videoFiles) > 0:
-    createFolder(videoPath)
-    orderedVideoFiles = getFileInfo(videoFiles)
-    # Process video files
-    processMediaFiles(orderedVideoFiles, videoPath)
+ic(getOldestDateFromFiles(photoFiles))
